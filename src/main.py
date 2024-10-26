6e59222e7eeaa1aa56c290de13d24fa4cbf62ac5
Juan Zamudio <jzamudio14@gmail.com>
2024-10-26 11:00:44 -0400

    pushing

--- src/main.py
+++ src/main.py
@@ -23,28 +23,28 @@ print(message.sid)
 
 def main():
 
-app = FastAPI(
-    title="Test TREX 2",
-    description="This is a testing web app for TREX 2",
-    version="0.1.0",
-)
+  app = FastAPI(
+      title="Test TREX 2",
+      description="This is a testing web app for TREX 2",
+      version="0.1.0",
+  )
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
-
-
-@app.get("/", response_class=HTMLResponse)
-async def root(request: Request):
-    return templates.TemplateResponse(
-        request=request, name="index.html", context={"request": request}
-    )
+  app.mount("/static", StaticFiles(directory="static"), name="static")
+  templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/results/{result_id}")
-async def read_results(result_id):
-    return {"identified": result_id}
+  @app.get("/", response_class=HTMLResponse)
+  async def root(request: Request):
+      return templates.TemplateResponse(
+          request=request, name="index.html", context={"request": request}
+      )
 
 
-@app.get("/favicon.ico")
-async def favicon():
-    return FileResponse("static/imgs/ca_icon.ico")
+  @app.get("/results/{result_id}")
+  async def read_results(result_id):
+      return {"identified": result_id}
+
+
+  @app.get("/favicon.ico")
+  async def favicon():
+      return FileResponse("static/imgs/ca_icon.ico")
