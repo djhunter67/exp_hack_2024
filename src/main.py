649dfa1ef437fff04ce783dc0f722f470cb373fa
Juan Zamudio <jzamudio14@gmail.com>
2024-10-26 10:58:01 -0400

    fixing conflicts

--- src/main.py
+++ src/main.py
@@ -7,6 +7,13 @@ from twilio.rest import Client
 
 account_sid = 'ACa8a8854a798b8589d2922edff928e5bb'
 auth_token = 'fde69572abba08fde04f270d4589c67b'
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # load environment variables from .env file
+
+account_sid = os.getenv("ACCOUNT_SID")
+auth_token = os.getenv("AUTH_TOKEN")
 
 client = Client(account_sid, auth_token)
 
